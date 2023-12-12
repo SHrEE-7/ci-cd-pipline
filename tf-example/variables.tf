144eedcfb911e7d82d53c4ff0fb84578b11f7510
SHrEE-7 <shreekumarf@gmail.com>
2023-12-12 11:49:34 +0530

    variables

--- tf-example/variables.tf
+++ tf-example/variables.tf
@@ -1,3 +1,6 @@
-variable "ec2_name" {
-  type = string
-}
+variable "env_prefix" {}
+variable "vpc_cider_block" {}
+variable "subnet_cider_block" {}
+variable "avail_zone" {}
+variable "instance_type" {}
+variable "ssh_private_key" {}
