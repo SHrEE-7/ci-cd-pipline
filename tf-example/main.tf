eca0c0ac3725478977392f91a61b1eb8cf858797
SHrEE-7 <shreekumarf@gmail.com>
2023-12-12 09:16:41 +0530

    terrafrom files

---
+++ tf-example/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" {
+    most_recent = true
+filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
+filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+owners = ["099720109477"] # Canonical
+}
+# provision to ap-south-1 region
+provider "aws" {
+  region  = "ap-south-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "app-ssh-key"
+tags = {
+    Name = var.ec2_name
+  }
+}
